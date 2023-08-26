812f3a70cd56de101e36bff00b49239ae5d63960
5u00n <surenhembram97@gmail.com>
2023-08-26 21:07:32 +0530

    farmer plant verification activity added to admin

---
+++ app/src/main/java/com/ashish/contractedfarming/Admin/Dashboard/Farmer/FarmerProfile/Profile/FarmerPlant/AdminFarmerPlantActivity.java
@@ -0,0 +1,16 @@
+package com.ashish.contractedfarming.Admin.Dashboard.Farmer.FarmerProfile.Profile.FarmerPlant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ashish.contractedfarming.R;
+
+public class AdminFarmerPlantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_farmer_plant);
+    }
+}
