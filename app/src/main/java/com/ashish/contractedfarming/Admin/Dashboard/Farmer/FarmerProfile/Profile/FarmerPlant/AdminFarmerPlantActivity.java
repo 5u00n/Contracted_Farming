07a7ef916e70fa6d70a21e948b2aedc85029083e
5u00n <surenhembram97@gmail.com>
2023-09-06 02:54:47 +0530

    accept farmer plant ready for admin section

--- app/src/main/java/com/ashish/contractedfarming/Admin/Dashboard/Farmer/FarmerProfile/Profile/FarmerPlant/AdminFarmerPlantActivity.java
+++ app/src/main/java/com/ashish/contractedfarming/Admin/Dashboard/Farmer/FarmerProfile/Profile/FarmerPlant/AdminFarmerPlantActivity.java
@@ -4,17 +4,27 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.ashish.contractedfarming.Admin.Dashboard.News.AdminNewsModel;
+import com.ashish.contractedfarming.Models.NotificationModel;
+import com.ashish.contractedfarming.Models.RequestModel;
 import com.ashish.contractedfarming.R;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -23,6 +33,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.google.gson.Gson;
 import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class AdminFarmerPlantActivity extends AppCompatActivity {
 
@@ -53,7 +66,7 @@ public class AdminFarmerPlantActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_farmer_plant);
 
-        context=getBaseContext();
+        context=AdminFarmerPlantActivity.this;
 
         farmer_plant_id=getIntent().getExtras().getString("farmer_plant_id");
 
@@ -138,7 +151,7 @@ public class AdminFarmerPlantActivity extends AppCompatActivity {
             }
         });
 
-        textViewFarm.setOnClickListener(new View.OnClickListener() {
+        textViewFarmer.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -153,11 +166,181 @@ public class AdminFarmerPlantActivity extends AppCompatActivity {
         });
 
 
+        imageViewPlant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        textViewPlant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonPlant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imageViewFarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        textViewFarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonFarm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        imageViewManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        textViewManger.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        buttonManager.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        buttonAdminApproval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //LayoutInflater li = LayoutInflater.from(context);
+                //View promptsView = li.inflate(R.layout.prompt_add_news, null);
+
+                AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(context);
+
+                alertDialogBuilder.setTitle("Approve or Reject a Farmers Plant Request !");
+
+                // set prompts.xml to alertdialog builder
+                //alertDialogBuilder.setView(promptsView);
+
+
+                // set dialog message
+
+                String noti_id="noti_"+farmer_plant_id;
+
+                alertDialogBuilder.setCancelable(false).setPositiveButton("Approve", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        reference.child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("Approved");
+                        reference.child("users").child("farmer").child(textViewFarmer.getText().toString()).child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("Approved");
+
+                        reference.child("users").child("farmer").child(farmerId.getText().toString()).child("notifications").child(noti_id).setValue(new NotificationModel(noti_id,"admin", auth.getUid(), "Approved your Plant Request on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "farmer_plant", "false"));
+                        reference.child("users").child("manager").child(managerId.getText().toString()).child("notifications").child(noti_id).setValue(new NotificationModel(noti_id,"admin", auth.getUid(), "Approved a Plant Request of "+textViewFarmer.getText().toString()+"(farmer) on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "farmer_plant", "false"));
+                        dialog.cancel();
+                    }
+                }).setNegativeButton("Reject", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        reference.child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("Rejected");
+                        reference.child("users").child("farmer").child(textViewFarmer.getText().toString()).child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("Rejected");
+
+                        reference.child("users").child("farmer").child(farmerId.getText().toString()).child("notifications").child(noti_id).setValue(new NotificationModel(noti_id,"admin", auth.getUid(), "Rejected your Plant Request on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "farmer_plant", "false"));
+                        reference.child("users").child("manager").child(managerId.getText().toString()).child("notifications").child(noti_id).setValue(new NotificationModel(noti_id,"admin", auth.getUid(), "Rejected a Plant Request of "+textViewFarmer.getText().toString()+"(farmer) on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "farmer_plant", "false"));
+
+                        dialog.cancel();
+                    }
+                }).setNeutralButton("Keep on Hold", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        reference.child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("On Hold");
+                        reference.child("users").child("farmer").child(textViewFarmer.getText().toString()).child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("On Hold");
+
+                        reference.child("users").child("farmer").child(farmerId.getText().toString()).child("notifications").child(noti_id).setValue(new NotificationModel(noti_id,"admin", auth.getUid(), "put your Plant Request on hold on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "farmer_plant", "false"));
+                        reference.child("users").child("manager").child(managerId.getText().toString()).child("notifications").child(noti_id).setValue(new NotificationModel(noti_id,"admin", auth.getUid(), "put a Plant Request of "+textViewFarmer.getText().toString()+"(farmer) on hold on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "farmer_plant", "false"));
+                        dialogInterface.cancel();
+                    }
+                });
+
+                // create alert dialog
+                AlertDialog alertDialog = alertDialogBuilder.create();
+                // show it
+                alertDialog.show();
+                /*if(!textViewAdminApproval.getText().toString().equals("Approved")) {
+                    reference.child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("Approved");
+                }else{
+                    reference.child("farmer_plants").child(farmer_plant_id).child("approval_admin").setValue("Rejected");
+                }
+
+                 */
+            }
+        });
+
+
+        buttonManagerApproval.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MaterialAlertDialogBuilder alertDialogBuilder= new MaterialAlertDialogBuilder(context);
+
+                alertDialogBuilder.setTitle("Yor are sending request to the concerned Manger For Approval !");
+
+                alertDialogBuilder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        String time_stamp=String.valueOf(Calendar.getInstance().getTime().getTime() / 1000);
+                        RequestModel requestModel = new RequestModel("requests_" + auth.getUid() + "_" + time_stamp, auth.getUid(), "manager", "plant_request_" + textViewPlant, farmer_plant_id, time_stamp, "00", "00", "Hello from Admin, Please check this in detail, and proceed .");
+
+                        reference.child("users").child("manager").child(managerId.getText().toString()).child("requests").child("requests_" + auth.getUid() + "_" + time_stamp).setValue(requestModel);
+
+                        reference.child("users").child("manager").child(managerId.getText().toString()).child("notifications").child("noti_"+farmer_plant_id).setValue(new NotificationModel("noti_"+farmer_plant_id,"admin", auth.getUid(), "requested you to check from "+textViewFarmer.getText().toString()+"(farmer) on hold on the date : " + new SimpleDateFormat("dd MMM YYYY hh:mm a").format(Calendar.getInstance().getTime().getTime()) + " click to check ! ", String.valueOf(Calendar.getInstance().getTime().getTime() / 1000), "request", "false"));
+                    }
+                }).setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        dialogInterface.cancel();
+                    }
+                });
+
+                androidx.appcompat.app.AlertDialog dialog= alertDialogBuilder.create();
+                dialog.show();
+
+
+            }
+        });
+
+        imageViewVerification.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
 
     }
 
     void getDataFromFirebase(){
-        reference.child("farmer_plants").child(farmer_plant_id).addListenerForSingleValueEvent(new ValueEventListener() {
+        reference.child("farmer_plants").child(farmer_plant_id).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
@@ -186,7 +369,7 @@ public class AdminFarmerPlantActivity extends AppCompatActivity {
                     textViewAdminApproval.setText(snapshot.child("approval_admin").getValue().toString());
                     textViewManagerApproval.setText(snapshot.child("approval_manager").getValue().toString());
 
-                    textViewDateAdded.setText(snapshot.child("date_added").getValue().toString());
+                    textViewDateAdded.setText(new SimpleDateFormat("dd MMM yyyy hh:mm a").format(Float.parseFloat(snapshot.child("date_added").getValue().toString())*1000));
                     textViewFinalApproval.setText("By __manager on dd mm yyy");//snapshot.child("final_approval").getValue().toString());
 
                     if(snapshot.child("manager_loc").exists()) {
